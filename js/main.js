70c51f9b1df97d9eed5cbdde775cfe37b2f4ac98
unknown <kswedberg@nossi.edu>
2018-06-04 16:01:50 -0500

    hey

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var quotes = ["Saddles are for wussies.", "The bigger the ears, the better.","C","D","E","F"];
+var quotes = ["Saddles are for wussies.", "The bigger the ears, the better.","He's only half a butt.","Galloping is hard. Bucking is better.","Salt blocks are life.","He's a hooved canine."];
 
 $('#button').click(function(){
 	$('#quoteArea').html(quotes[Math.floor(Math.random() * quotes.length)] );
